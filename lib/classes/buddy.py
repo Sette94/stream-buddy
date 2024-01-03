76651439167dbfb2507ef91afeaecfb83f5f800e
nsette <nicholas.sette@mlb.com>
2024-01-03 01:01:27 -0500

    added Tv!

--- lib/classes/buddy.py
+++ lib/classes/buddy.py
@@ -6,6 +6,7 @@ from actor import Actor
 from rich.console import Console
 from rich.table import Table
 from rich import box
+from rich.style import Style
 
 
 FORMAT = '%(asctime)-15s %(filename)s - %(lineno)d - %(message)s'
@@ -55,22 +56,77 @@ class Buddy:
 
         return movies_by_service.get("results")
 
-    def display_table(self, movies):
+    def tv_in_streaming(self, year="", genre_name=""):
+
+        streaming_id = self.streaming_config.streaming_service_id(
+            self.streaming_service)
+
+        tv_by_service = self.streaming_config.tv_by_streaming_service(
+            streaming_id,
+            year,
+            self.genre_config.genre_id(genre_name))
+
+        return tv_by_service.get("results")
+
+    def display_table_movies(self, movies):
         table = Table(title=self.streaming_service,
                       box=box.HEAVY, show_lines=True)
 
+        title_style = Style(color="White", bold=True, underline=True)
+        table.title_style = title_style
+
         rows = [
-            [movie.get('original_title', 'N/A'), movie.get('overview',
-                                                           'N/A'), movie.get('release_date', 'N/A')]
+            [movie.get('original_title', 'N/A'), movie.get('overview', 'N/A'),
+             movie.get('release_date', 'N/A'), movie.get('poster_path', 'N/A')]
             for movie in movies
         ]
 
-        columns = ["Title", "Overview", "Release Date"]
+        columns = ["Title", "Plot", "Release Date", "Poster"]
 
         for column in columns:
             table.add_column(column, justify="center", style="red")
 
         for row in rows:
+            # Make each cell in the "Poster" column clickable with the corresponding URL
+            for i, cell_value in enumerate(row):
+                if columns[i] == "Poster":
+                    cell_text = "https://image.tmdb.org/t/p/original"+cell_value
+                    row[i] = cell_text
+                else:
+                    row[i] = cell_value
+
+            table.add_row(*row)
+
+        console = Console()
+        console.print(table)
+
+    def display_table_tv(self, tvs):
+        table = Table(title=self.streaming_service,
+                      box=box.HEAVY, show_lines=True)
+
+        title_style = Style(color="White", bold=True, underline=True)
+        table.title_style = title_style
+
+        rows = [
+            [tv.get('original_name', 'N/A'), tv.get('overview', 'N/A'),
+             tv.get('first_air_date', 'N/A'), tv.get('poster_path', 'N/A')]
+            for tv in tvs
+        ]
+
+        columns = ["Title", "Plot", "Release Date", "Poster"]
+
+        for column in columns:
+            table.add_column(column, justify="center", style="red")
+
+        for row in rows:
+            # Make each cell in the "Poster" column clickable with the corresponding URL
+            for i, cell_value in enumerate(row):
+                if columns[i] == "Poster":
+                    cell_text = "https://image.tmdb.org/t/p/original"+cell_value
+                    row[i] = cell_text
+                else:
+                    row[i] = cell_value
+
             table.add_row(*row)
 
         console = Console()
@@ -78,6 +134,11 @@ class Buddy:
 
 
 buddy_instance = Buddy(streaming_service="Netflix")
+tv_data = buddy_instance.tv_in_streaming(
+    year="1999", genre_name=""
+)
+buddy_instance.display_table_tv(tv_data)
+
 movies_data = buddy_instance.movies_in_streaming(
-    genre_name="", year="2018")
-buddy_instance.display_table(movies_data)
+    genre_name="", year="2023", actor_name="")
+buddy_instance.display_table_movies(movies_data)
--- lib/classes/streaming.py
+++ lib/classes/streaming.py
@@ -28,11 +28,27 @@ class Streaming:
         try:
             streaming_services = requests.get(
                 self.base_props.get_property(
-                    'streaming_endpoint') +
+                    'movie_streaming_endpoint') +
                 f"&with_watch_providers={streaming_id}" +
                 f"&primary_release_year={year}" +
                 f"&with_genres={genre_id}" +
                 f"&with_cast={actor_id}",
+                headers=self.base_props.get_property('tmdb_headers'))
+
+            return streaming_services.json()
+        except Exception as e:
+            logger.error(e)
+            return
+
+    def tv_by_streaming_service(self, streaming_id, year="", genre_id=""):
+
+        try:
+            streaming_services = requests.get(
+                self.base_props.get_property(
+                    'tv_streaming_endpoint') +
+                f"&with_watch_providers={streaming_id}" +
+                f"&first_air_date_year={year}" +
+                f"&with_genres={genre_id}",
                 headers=self.base_props.get_property('tmdb_headers'))
 
             return streaming_services.json()
