dda6c1821de0ffebf37d6f6c67473298533c61a3
Gabriel Wortmann <wortmanngabriel@gmail.com>
2024-01-03 15:56:06 -0600

    partial property setup for user

--- lib/user.py
+++ lib/user.py
@@ -4,11 +4,30 @@
 from connection import CONN, CURSOR
 
 class User:
-    def __init__(self, first_name, last_name, age, id=None):
+    def __init__(self, first_name: str, last_name: str, age: int, id: int=None):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         self.id = id
+
+    @property
+    def first_name(self):
+        return self.first_name
+    
+    @first_name.setter
+    def first_name(self, name: str):
+        if isinstance(name, str) and len(name) > 0:
+            self.first_name = name
+    
+    @property
+    def last_name(self):
+        return self.last_name
+    
+    @last_name.setter
+    def last_name(self, name: str):
+        if isinstance(name, str) and len(name) > 0:
+            self.last_name = name
+
 
     #creating table
     @classmethod
