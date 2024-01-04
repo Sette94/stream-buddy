3849343d0c1bb648efd303c813685dbdd3709a46
nsette <nicholas.sette@mlb.com>
2024-01-04 11:29:32 -0500

    Adding username

--- lib/favorite.py
+++ lib/favorite.py
@@ -1,32 +1,33 @@
 from connection import CONN, CURSOR
 
+
 class Favorite:
     def __init__(self, movie_name: str, rating: int, user_id=None, id=None):
         self.movie_name = movie_name
         self.rating = rating
         self.user_id = user_id
         self.id = id
-    
+
     @property
     def movie_name(self):
         return self.movie_name
-    
+
     @movie_name.setter
     def movie_name(self, name: str):
         if isinstance(name, str) and len(name) > 0:
             self.movie_name = name
-    
+
     @property
     def rating(self):
         return self.rating
-    
+
     @rating.setter
     def rating(self, num: int):
         if isinstance(num, int) and num > 0:
             self.rating = num
 
+    # creating table
 
-    #creating table
     @classmethod
     def create_table(cls):
         sql = """
@@ -41,7 +42,7 @@ class Favorite:
         """
         CURSOR.execute(sql)
         CONN.commit()
-    
+
     @classmethod
     def drop_table(cls):
         sql = """
@@ -50,8 +51,8 @@ class Favorite:
         CURSOR.execute(sql)
         CONN.commit()
 
-    #creating instance of the favorite class
-    @classmethod 
+    # creating instance of the favorite class
+    @classmethod
     def favorite_instance(cls, row):
         favorite = cls(
             id=row[0],
@@ -59,9 +60,9 @@ class Favorite:
             rating=row[2],
             user_id=row[3]
         )
-        return favorite 
+        return favorite
 
-    #saving the values to each instance which means I do not use a classmethod
+    # saving the values to each instance which means I do not use a classmethod
     def saving_favorite_data(self):
         sql = """
             INSERT INTO favorites(movie_name, rating, user_id)
@@ -70,37 +71,38 @@ class Favorite:
         CURSOR.execute(sql, (self.movie_name, self.rating, self.user_id))
         CONN.commit()
         self.id = CURSOR.lastrowid
-    
-    #get all rows aka instances
-    ##need class methos because I am working with the whole class
+
+    # get all rows aka instances
+    # need class methos because I am working with the whole class
     @classmethod
     def get_all(cls):
-           sql = """ 
+        sql = """ 
            SELECT * FROM favorites;
            """
-           rows = CURSOR.execute(sql).fetchall() 
-           #DON'T NEED CONN.COMMT() BECAUSE NOTHIG IS BEING UPDATED JUST READING DATA
-           #turn list into list of instances 
-           return [cls.favorite_instance(row) for row in rows]
+        rows = CURSOR.execute(sql).fetchall()
+        # DON'T NEED CONN.COMMT() BECAUSE NOTHIG IS BEING UPDATED JUST READING DATA
+        # turn list into list of instances
+        return [cls.favorite_instance(row) for row in rows]
 
+    # getting instance aka row by id
+    # need class method because I am working with the whole class
 
-    #getting instance aka row by id
-    #need class method because I am working with the whole class
     @classmethod
+    # When we call this method we would first call user.find_userid_by_user_name to get the id of the username
     def find_favorite_by_id(cls, id):
         sql = """ 
             SELECT * FROM favorites WHERE id=?;
         """
-        row = CURSOR.execute(sql, (id, )).fetchone() 
-        #DON'T NEED CONN.COMMT() BECAUSE NOTHIG IS BEING UPDATED JUST READING DATA
+        row = CURSOR.execute(sql, (id, )).fetchone()
+        # DON'T NEED CONN.COMMT() BECAUSE NOTHIG IS BEING UPDATED JUST READING DATA
         if not row:
             return None
         else:
             return cls.favorite_instance(row)
 
-    #deleting instance aka row by id
-    #again need class because I am working with the whole class
-    #but note can also work with an instance
+    # deleting instance aka row by id
+    # again need class because I am working with the whole class
+    # but note can also work with an instance
     @classmethod
     def delete_by_id(cls, id):
         sql = """ 
@@ -109,84 +111,84 @@ class Favorite:
         CURSOR.execute(sql, (id, ))
         CONN.commit()
 
-    #this is an instance method because it is going to operate on a single instance not the whole table
+    # this is an instance method because it is going to operate on a single instance not the whole table
     def add_user(self, user_id):
         try:
-        # Check if the user with the specified ID exists
+            # Check if the user with the specified ID exists
             user_exists_sql = """
                 SELECT id FROM users WHERE id=?;
             """
-            user_exists = CURSOR.execute(user_exists_sql, (user_id,)).fetchone()
+            user_exists = CURSOR.execute(
+                user_exists_sql, (user_id,)).fetchone()
 
             if user_exists:
-            # If user exists, update the favorite
+                # If user exists, update the favorite
                 update_sql = """ 
                     UPDATE favorites SET user_id=? WHERE id=?;
                 """
                 CURSOR.execute(update_sql, (user_id, self.id))
                 CONN.commit()
                 self.user_id = user_id
-                print(f"User with ID {user_id} associated with Favorite with ID {self.id} successfully.")
+                print(
+                    f"User with ID {user_id} associated with Favorite with ID {self.id} successfully.")
             else:
-                print(f"User with ID {user_id} does not exist. No update performed.")
+                print(
+                    f"User with ID {user_id} does not exist. No update performed.")
         except Exception as e:
             print(f'Something went wrong: {e}')
 
 
-        
-#for testing
+# for testing
 if __name__ == "__main__":
-    
-    #Favorite.create_table()
-    #Favorite.drop_table()
 
+    # Favorite.create_table()
+    # Favorite.drop_table()
 
-    #add data to the favorites table
-    #favorite_instance = Favorite(movie_name='AAA', rating=5)
-    #favorite_instance.saving_favorite_data()
+    # add data to the favorites table
+    # favorite_instance = Favorite(movie_name='AAA', rating=5)
+    # favorite_instance.saving_favorite_data()
 
-    #favorite_instance = Favorite(movie_name='BBB', rating=2)
-    #favorite_instance.saving_favorite_data()
+    # favorite_instance = Favorite(movie_name='BBB', rating=2)
+    # favorite_instance.saving_favorite_data()
 
-    #favorite_instance = Favorite(movie_name='CCC', rating=1)
-    #favorite_instance.saving_favorite_data()
+    # favorite_instance = Favorite(movie_name='CCC', rating=1)
+    # favorite_instance.saving_favorite_data()
 
-    #it works!
-    #Testing get_all
-    #all_favorites = Favorite.get_all()
+    # it works!
+    # Testing get_all
+    # all_favorites = Favorite.get_all()
 
-    #if all_favorites:
-        #for favorite in all_favorites:
-            #print(f"Favorite instance: {favorite.__dict__}")
-    #else:
-        #print("No favorites found.")
+    # if all_favorites:
+    # for favorite in all_favorites:
+    # print(f"Favorite instance: {favorite.__dict__}")
+    # else:
+    # print("No favorites found.")
 
     # it works!
     # Testing get_by_id
-    #retrieved_favorite = Favorite.find_favorite_by_id(2)
-    #if retrieved_favorite:
-        #print(f"Favorite with id 3: {retrieved_favorite.__dict__}")
-    #else:
-        #print("Favorite not found.")
+    # retrieved_favorite = Favorite.find_favorite_by_id(2)
+    # if retrieved_favorite:
+    # print(f"Favorite with id 3: {retrieved_favorite.__dict__}")
+    # else:
+    # print("Favorite not found.")
 
     # Testing delete_by_id
     # Print all instances before deletion - run this to see list before deleting one
-    #print("Before Deletion:")
-    #for favorite in Favorite.get_all():
-        #print(f"ID: {favorite.id}, Movie Name: {favorite.movie_name}, Rating: {favorite.rating}")
-    
+    # print("Before Deletion:")
+    # for favorite in Favorite.get_all():
+    # print(f"ID: {favorite.id}, Movie Name: {favorite.movie_name}, Rating: {favorite.rating}")
+
     # It works!
     # Choosing the ID I want to delete
-    #favorite_id_to_delete = 2
+    # favorite_id_to_delete = 2
 
     # Delete the instance with the specified ID
-    #Favorite.delete_by_id(favorite_id_to_delete)
+    # Favorite.delete_by_id(favorite_id_to_delete)
 
     # Print all instances after deletion
-    #print("\nAfter Deletion:")
-    #for favorite in Favorite.get_all():
-        #print(f"ID: {favorite.id}, Movie Name: {favorite.movie_name}, Rating: {favorite.rating}")
-    
+    # print("\nAfter Deletion:")
+    # for favorite in Favorite.get_all():
+    # print(f"ID: {favorite.id}, Movie Name: {favorite.movie_name}, Rating: {favorite.rating}")
 
     # testing user instance passed to favorite
     # Assuming you have instances of Favorite with ID 2 and a User with ID 3
@@ -200,7 +202,8 @@ if __name__ == "__main__":
         # Add the user (with ID 3) to the favorite
         favorite_instance.add_user(user_id_for_testing)
         # printing message
-        print(f"User ID associated with Favorite ID 2: {favorite_instance.user_id}")
+        print(
+            f"User ID associated with Favorite ID 2: {favorite_instance.user_id}")
     else:
         # printing error message
         print("Favorite not found.")
--- lib/user.py
+++ lib/user.py
@@ -1,25 +1,27 @@
 from connection import CONN, CURSOR
 
+
 class User:
-    def __init__(self, first_name: str, last_name: str, age: int, id=None):
+    def __init__(self, first_name: str, last_name: str, age: int, user_name: str, id=None):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+        self.user_name = user_name
         self.id = id
 
     @property
     def first_name(self):
         return self.first_name
-    
+
     @first_name.setter
     def first_name(self, name: str):
         if isinstance(name, str) and len(name) > 0:
             self.first_name = name
-    
+
     @property
     def last_name(self):
         return self.last_name
-    
+
     @last_name.setter
     def last_name(self, name: str):
         if isinstance(name, str) and len(name) > 0:
@@ -28,15 +30,24 @@ class User:
     @property
     def age(self):
         return self.age
-    
+
     @age.setter
     def age(self, num: int):
         if isinstance(num, int) and num > 0:
             self.age = num
 
+    # Setting property for username
+    @property
+    def user_name(self):
+        return self.user_name
 
+    @user_name.setter
+    def user_name(self, name: str):
+        if isinstance(name, str) and len(name) > 0 and not hasattr(self, "user_name"):
+            self.user_name = name
 
-    #creating table
+    # creating table
+
     @classmethod
     def create_table(cls):
         sql = """
@@ -45,12 +56,13 @@ class User:
                 id INTEGER PRIMARY KEY,
                 first_name TEXT,
                 last_name TEXT,
-                age INTEGER
+                age INTEGER,
+                user_name TEXT
             );
         """
         CURSOR.execute(sql)
         CONN.commit()
-    
+
     @classmethod
     def drop_table(cls):
         sql = """
@@ -59,107 +71,122 @@ class User:
         CURSOR.execute(sql)
         CONN.commit()
 
-    #creating instance of the user class
-    @classmethod 
+    # creating instance of the user class
+    @classmethod
     def user_instance(cls, row):
         user = cls(
             id=row[0],
             first_name=row[1],
             last_name=row[2],
             age=row[3],
+            user_name=row[4]
         )
-        return user 
-        
+        return user
+
     def user_data(self):
         sql = """
-            INSERT INTO users(first_name, last_name, age)
-            VALUES(?, ?, ?);
+            INSERT INTO users(first_name, last_name, age,user_name)
+            VALUES(?, ?, ?,?);
         """
-        CURSOR.execute(sql, (self.first_name, self.last_name, self.age))
+        CURSOR.execute(sql, (self.first_name, self.last_name,
+                       self.age, self.user_name))
         CONN.commit()
-    
-    #get all rows aka instances
-    ##need class methos because I am working with the whole class
+
+    # get all rows aka instances
+    # need class methos because I am working with the whole class
     @classmethod
     def get_all(cls):
-           sql = """ 
+        sql = """ 
            SELECT * FROM users;
            """
-           rows = CURSOR.execute(sql).fetchall() 
-           #turn list into list of instances 
-           return [cls.user_instance(row) for row in rows]
+        rows = CURSOR.execute(sql).fetchall()
+        # turn list into list of instances
+        return [cls.user_instance(row) for row in rows]
 
+    # getting instance aka row by id
+    # need class method because I am working with the whole class
 
-    #getting instance aka row by id
-    #need class method because I am working with the whole class
     @classmethod
     def find_user_by_id(cls, id):
         sql = """ 
             SELECT * FROM users WHERE id=?;
         """
-        row = CURSOR.execute(sql, (id, )).fetchone() 
+        row = CURSOR.execute(sql, (id, )).fetchone()
         if not row:
             return None
         else:
             return cls.user_instance(row)
 
-    #deleting instance aka row by id
-    #again need class because I am working with the whole class
+    # Pass in user_name and return id
+
+    def find_userid_by_user_name(cls, user_name):
+        sql = """ 
+            SELECT id FROM users WHERE user_name=?;
+        """
+        row = CURSOR.execute(sql, (user_name, )).fetchone()
+        if not row:
+            return None
+        else:
+            return cls.user_instance(row)
+
+    # deleting instance aka row by id
+    # again need class because I am working with the whole class
+
     @classmethod
-    def delete_by_id(cls,id):
+    def delete_by_id(cls, id):
         sql = """ 
         DELETE FROM users WHERE id=?;
         """
         CURSOR.execute(sql, (id, ))
         CONN.commit()
 
-    
-#if __name__ == "__main__":
-    #User.create_table()
-    #User.drop_table()
 
-    #Create an instance of the User class and add data to the users table
-    #user_instance = User(first_name='John', last_name='Doe', age=25)
-    #user_instance.user_data()
+# if __name__ == "__main__":
+    # User.create_table()
+    # User.drop_table()
 
-    #user_instance = User(first_name='Jane', last_name='Doe', age=30)
-    #user_instance.user_data()
+    # Create an instance of the User class and add data to the users table
+    # user_instance = User(first_name='John', last_name='Doe', age=25)
+    # user_instance.user_data()
 
-    #user_instance = User(first_name='June', last_name='Doe', age=54)
-    #user_instance.user_data()
+    # user_instance = User(first_name='Jane', last_name='Doe', age=30)
+    # user_instance.user_data()
 
-    #it works!
-    #Testing get_all
-    #all_users = User.get_all()
+    # user_instance = User(first_name='June', last_name='Doe', age=54)
+    # user_instance.user_data()
 
-    #if all_users:
-        #for user in all_users:
-            #print(f"User instance: {user.__dict__}")
-    #else:
-        #print("No users found.")
+    # it works!
+    # Testing get_all
+    # all_users = User.get_all()
 
-    #it works!
-    #Testing get_by_id
-    #retrieved_user = User.find_user_by_id(2)
-    #if retrieved_user:
-        #print(f"User with id 2: {retrieved_user.__dict__}")
-    #else:
-        #print("User not found.")
+    # if all_users:
+        # for user in all_users:
+        # print(f"User instance: {user.__dict__}")
+    # else:
+        # print("No users found.")
 
-    #Testing delete_by_id
-    #Print all instances before deletion - run this to see list before deleting one
-    #print("Before Deletion:")
-    #for user in User.get_all():
-        #print(f"ID: {user.id}, User First Name: {user.first_name}, User Last Name: {user.last_name}")
-    
+    # it works!
+    # Testing get_by_id
+    # retrieved_user = User.find_user_by_id(2)
+    # if retrieved_user:
+        # print(f"User with id 2: {retrieved_user.__dict__}")
+    # else:
+        # print("User not found.")
+
+    # Testing delete_by_id
+    # Print all instances before deletion - run this to see list before deleting one
+    # print("Before Deletion:")
+    # for user in User.get_all():
+        # print(f"ID: {user.id}, User First Name: {user.first_name}, User Last Name: {user.last_name}")
+
     # It works!
     # Choosing the ID I want to delete
-    #user_id_to_delete = 2
+    # user_id_to_delete = 2
 
     # Delete the instance with the specified ID
-    #User.delete_by_id(user_id_to_delete)
+    # User.delete_by_id(user_id_to_delete)
 
     # Print all instances after deletion
-    #print("\nAfter Deletion:")
-    #for user in User.get_all():
-        #print(f"ID: {user.id}, User First Name: {user.first_name}, User Last Name: {user.last_name}")
+    # print("\nAfter Deletion:")
+    # for user in User.get_all():
+        # print(f"ID: {user.id}, User First Name: {user.first_name}, User Last Name: {user.last_name}")
